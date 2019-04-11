bd0a423c6024e08c21bc60b3cef912ed4378f4fb
jn8029 <warren.y.cheng@gmail.com>
2019-04-11 14:18:31 +0800

    algorithm completed, working on options

--- options.js
+++ options.js
@@ -4,18 +4,11 @@
 
 'use strict';
 
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
-}
-constructOptions(kButtonColors);
+let vocab_selector = document.getElementById('vocabulary-selector');
+vocab_selector.addEventListener("change", function(){
+  let wordlist = vocab_selector.value
+  chrome.storage.local.set({'wordlist': wordlist}, function() {
+          console.log('Word list is set to ' + wordlist);
+        });
+
+})
