7634cabec563b7e5497951eceed00a88a4160a84
jn8029 <warren.y.cheng@gmail.com>
2019-04-12 23:26:08 +0800

    add icons, revise manifest, tidy up options.html and js

--- options.js
+++ options.js
@@ -11,3 +11,18 @@ vocab_selector.addEventListener("change", function(){
           console.log('Word list is set to ' + wordlist);
         });
 })
+
+document.getElementById("save-btn").addEventListener("click",function(e){
+  e.preventDefault()
+
+  console.log("clicked!")
+
+  var b = document.getElementById("save-status");
+  b.style.setProperty("-webkit-transition","opacity 0s ease-out");
+  b.style.opacity = 1;
+  window.setTimeout(function() {
+      b.style.setProperty("-webkit-transition", "opacity 1s ease-out");
+      b.style.opacity = 0
+  }, 1500);
+
+})
