2b3e5349e2f9d85117865e82e1ba4470e32e0b07
jn8029 <warren.y.cheng@gmail.com>
2019-04-12 20:32:11 +0800

    add popup, html js

--- popup.js
+++ popup.js
@@ -4,18 +4,13 @@
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+document.getElementsByName("switch").forEach(
+  function(button){
+    button.addEventListener("click",function(target){
+      let selected_button = target.value
+      console.log(target.value)
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+    }
+  )
+})
